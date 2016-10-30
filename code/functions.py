8437f86adbdd41095b0a6e2701f37821d4162b4f
Karandeep Johar <karandeepjohar@gmail.com>
2016-10-30 18:21:41 -0400

    first commit

---
+++ code/functions.py
@@ -0,0 +1,40 @@
+# some useful functions
+import numpy as np
+from xman import *
+
+
+# some useful functions
+# declare all operations here first
+
+class f(XManFunctions):
+    @staticmethod
+    def square(a):
+        return XManFunctions.registerDefinedByOperator('square',a)
+
+# the functions that autograd.eval will use to evaluate each function,
+# to be called with the functions actual inputs as arguments
+
+EVAL_FUNS = {
+    'add':      lambda x1,x2: x1+x2,
+    'subtract': lambda x1,x2: x1-x2,
+    'square':   np.square,
+    }
+
+# the functions that autograd.bprop will use in reverse mode
+# differentiation.  BP_FUNS[f] is a list of functions df1,....,dfk
+# where dfi is used in propagating errors to the i-th input xi of f.
+# Specifically, dfi is called with the ordinary inputs to f, with two
+# additions: the incoming error, and the output of the function, which
+# was computed by autograd.eval in the eval stage.  dfi will return
+# delta * df/dxi [f(x1,...,xk)]
+# 
+# note: I don't have derivatives for crossEnt and softMax, instead we
+# will look for patterns of the form "z = crossEnt(softMax(x), y)" and
+# replace them with "z = crossEnt-softMax(x,y)", which we DO have a
+# derivative defined for.  
+
+BP_FUNS = {
+    'add':              [lambda delta,out,x1,x2: _derivAdd(delta,x1),    lambda delta,out,x1,x2: _derivAdd(delta,x2)],
+    'subtract':         [lambda delta,out,x1,x2: _derivAdd(delta,x1),    lambda delta,out,x1,x2: -_derivAdd(delta,x2)],
+    'square':           [lambda delta,out,x : delta * 2.0 * x],
+    }
---
+++ code/mlp.py
@@ -0,0 +1,89 @@
+"""
+Multilayer Perceptron for character level entity classification
+"""
+import argparse
+import numpy as np
+from xman import *
+from utils import *
+from autograd import *
+
+np.random.seed(0)
+EPS = 1e-4
+
+def glorot(m,n):
+    #TODO return scale for glorot initialization
+    pass
+
+class MLP(Network):
+    """
+    Multilayer Perceptron
+    Accepts list of layer sizes [in_size, hid_size1, hid_size2, ..., out_size]
+    """
+    def __init__(self, layer_sizes):
+        self.graph = self._build() # DO NOT REMOVE THIS LINE. Store the output of xman.setup() in this variable
+
+        #TODO add other instance variables here.
+
+
+def main(params):
+    epochs = params['epochs']
+    max_len = params['max_len']
+    num_hid = params['num_hid']
+    batch_size = params['batch_size']
+    dataset = params['dataset']
+    init_lr = params['init_lr']
+    output_file = params['output_file']
+
+    # load data and preprocess
+    dp = DataPreprocessor()
+    data = dp.preprocess('../data/%s.train'%dataset, '../data/%s.valid'%dataset, '../data/%s.test'%dataset)
+    # minibatches
+    mb_train = MinibatchLoader(data.training, batch_size, max_len, 
+           len(data.chardict), len(data.labeldict))
+    mb_valid = MinibatchLoader(data.validation, batch_size, max_len, 
+           len(data.chardict), len(data.labeldict))
+    mb_test = MinibatchLoader(data.test, batch_size, max_len, 
+           len(data.chardict), len(data.labeldict))
+
+    # build
+    print "building mlp..."
+    #TODO INITIALIZE MLP HERE
+
+
+    print "done"
+
+    # train
+    print "training..."
+    value_dict = mlp.graph.inputDict()
+    min_loss = 1e5
+    lr = init_lr
+    for i in range(epochs):
+        for (idxs,e,l) in mb_train:
+            #TODO prepare the input and do a fwd-bckwd pass over it and update the weights
+            pass
+        # validate
+        tot_loss, n= 0., 0
+        probs = []
+        targets = []
+        for (idxs,e,l) in mb_valid:
+            #TODO prepare the input and do a fwd pass over it to compute the loss
+            pass
+    print "done"
+
+    for (idxs,e,l) in mb_test:
+        # prepare input and do a fwd pass over it to compute the output probs
+        pass
+        
+    #np.save(output_file, ouput_probabilities)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--max_len', dest='max_len', type=int, default=10)
+    parser.add_argument('--num_hid', dest='num_hid', type=int, default=50)
+    parser.add_argument('--batch_size', dest='batch_size', type=int, default=16)
+    parser.add_argument('--dataset', dest='dataset', type=str, default='smaller')
+    parser.add_argument('--epochs', dest='epochs', type=int, default=20)
+    parser.add_argument('--init_lr', dest='init_lr', type=float, default=0.5)
+    parser.add_argument('--output_file', dest='output_file', type=str, default='output')
+    params = vars(parser.parse_args())
+    main(params)
